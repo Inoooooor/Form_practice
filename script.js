16f63c1a8c9e82a0ed522d62f3901773e0455483
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2022-12-01 17:45:02 +0300

    Added_color_toggling_deleted_useless_links

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const orangeThemeButton = document.querySelector(".orange_theme");
 const blueThemeButton = document.querySelector(".blue_theme");
 
 orangeThemeButton.addEventListener("click", function() {
-    document.body.classList.add("orange_theme");
+    document.documentElement.style.setProperty('--form-anchor-color', '#FF9C00');
 })
 blueThemeButton.addEventListener("click", function() {
-    document.body.classList.remove("orange_theme");
+    document.documentElement.style.setProperty('--form-anchor-color', '#0075ff');
 })

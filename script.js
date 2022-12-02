204cda6ab71bbe4e87363015f8b8d6c48a082afa
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2022-12-02 12:34:04 +0300

    Added_eng_translation

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const orangeThemeButton = document.querySelector(".orange_theme");
 const blueThemeButton = document.querySelector(".blue_theme");
 const ruButton = document.querySelector(".ru_button");
+const engButton = document.querySelector(".en_button");
 const formDiv = document.getElementById("form_div");
 
-// Translation toggling buttons
+// Themes toggling buttons
 orangeThemeButton.addEventListener("click", function() {
     document.documentElement.style.setProperty('--form-anchor-color', '#FF9C00');
 })
@@ -31,11 +32,36 @@ ruButton.addEventListener("click", function(){
     document.getElementById("russian_lang").innerHTML = "РУС";
     document.getElementById("eng_button").innerHTML = "АНГ";
 
-
     // Translating anchors
     let formAnchors = formDiv.getElementsByTagName("a");
     formAnchors[0].innerHTML = "Войти";
     formAnchors[1].innerHTML = "Пользовательскими условиями";
     formAnchors[2].innerHTML = "Политикой Конфиденциальности";
-
 })
+
+engButton.addEventListener("click", function() {
+    document.getElementById("page_title").innerHTML = "Form page";
+    document.getElementById("form_header").innerHTML = "Create an account";
+    document.getElementById("acc_question").innerHTML = "Already have an account?";
+    document.getElementById("email").innerHTML = "Email";
+    document.getElementById("first_name").innerHTML = "First name";
+    document.getElementById("last_name").innerHTML = "Last name";
+    document.getElementById("password").innerHTML = "Password";
+    document.getElementById("month").innerHTML = "Month";
+    document.getElementById("day").innerHTML = "Day";
+    document.getElementById("year").innerHTML = "Year";
+    document.querySelector(".birth_p").innerHTML = "Date of birth";
+    document.getElementById("agreement").innerHTML = "By Create account, I agree that I have read and accepted the ";
+    document.getElementById("and_word").innerHTML = " and ";
+    document.getElementById("submit").value = "Create account";
+    document.getElementById("russian_lang").innerHTML = "RUS";
+    document.getElementById("eng_button").innerHTML = "ENG";
+    
+    // Translating anchors
+    let formAnchors = formDiv.getElementsByTagName("a");
+    formAnchors[0].innerHTML = "Sign in";
+    formAnchors[1].innerHTML = "Terms of Use";
+    formAnchors[2].innerHTML = "Privacy Policy";
+    
+})
+// Translating back

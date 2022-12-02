a7c073f1173db14d0fa14bd131e6b45e8d296ac3
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2022-12-02 16:15:13 +0300

    Squashed commit of the following:
    commit 78fc73e748d824374248fb82edf035afa47c729d
    Author: Inoooooor <111541493+Inoooooor@users.noreply.github.com>
    Date:   Fri Dec 2 16:12:53 2022 +0300
    
        Added_toggling_color
    
    commit 16c7aeb7efefde65ab98133d59cb02ff965a04cb
    Author: Inoooooor <111541493+Inoooooor@users.noreply.github.com>
    Date:   Fri Dec 2 15:37:27 2022 +0300
    
        Deleted_console_logs
    
    commit 19b6396ae32f950e3fd5cbec529337c3e42db6a3
    Author: Inoooooor <111541493+Inoooooor@users.noreply.github.com>
    Date:   Fri Dec 2 15:26:58 2022 +0300
    
        Added_switching_to_lang_and_theme
    
    commit 6ed270251b69a7aa5bf97916291017e5fff1cd93
    Author: Inoooooor <111541493+Inoooooor@users.noreply.github.com>
    Date:   Fri Dec 2 14:43:02 2022 +0300
    
        Deleted_html_content
    
    commit 8f9a3b53dbaa5457192fdcf6de0d354a7e14f921
    Author: Inoooooor <111541493+Inoooooor@users.noreply.github.com>
    Date:   Fri Dec 2 14:01:12 2022 +0300
    
        Added_ternary_translation
    
    commit 449e4e05234444b20ddace5e52a6868617d8b4e8
    Merge: 7a16e23 204cda6
    Author: Inoooooor <111541493+Inoooooor@users.noreply.github.com>
    Date:   Fri Dec 2 12:48:51 2022 +0300
    
        Merge branch 'develop'
    
    commit 7a16e2390ed9453ffdc3ab653b797c367378a033
    Author: Inoooooor <111541493+Inoooooor@users.noreply.github.com>
    Date:   Thu Dec 1 17:34:12 2022 +0300
    
        Added_color_toggling_deleted_useless_links

--- script.js
+++ script.js
@@ -7,61 +7,61 @@ const formDiv = document.getElementById("form_div");
 // Themes toggling buttons
 orangeThemeButton.addEventListener("click", function() {
     document.documentElement.style.setProperty('--form-anchor-color', '#FF9C00');
+    orangeThemeButton.classList.toggle("grey_color");
+    blueThemeButton.classList.toggle("grey_color");
 })
 
 blueThemeButton.addEventListener("click", function() {
     document.documentElement.style.setProperty('--form-anchor-color', '#0075ff');
+    orangeThemeButton.classList.toggle("grey_color");
+    blueThemeButton.classList.toggle("grey_color");
 })
 
-// Translation itself
-ruButton.addEventListener("click", function(){
-    document.getElementById("page_title").innerHTML = "Регистрация";
-    document.getElementById("form_header").innerHTML = "Регистрация";
-    document.getElementById("acc_question").innerHTML = "Уже зарегистрированы?";
-    document.getElementById("email").innerHTML = "Почта";
-    document.getElementById("first_name").innerHTML = "Имя";
-    document.getElementById("last_name").innerHTML = "Фамилия";
-    document.getElementById("password").innerHTML = "Пароль";
-    document.getElementById("month").innerHTML = "Месяц";
-    document.getElementById("day").innerHTML = "День";
-    document.getElementById("year").innerHTML = "Год";
-    document.querySelector(".birth_p").innerHTML = "Дата рождения";
-    document.getElementById("agreement").innerHTML = "Нажимая кнопку Зарегистрироваться, Я соглашаюсь с ";
-    document.getElementById("and_word").innerHTML = " и ";
-    document.getElementById("submit").value = "Создать аккаунт";
-    document.getElementById("russian_lang").innerHTML = "РУС";
-    document.getElementById("eng_button").innerHTML = "АНГ";
-
+// Translation with ternary function
+let isRus = 0;
+function ternTranslation (lang_flag) {
+    document.getElementById("page_title").innerHTML = lang_flag ? "Регистрация" : "Form page";
+    document.getElementById("form_header").innerHTML = lang_flag ? "Регистрация" : "Create an account";
+    document.getElementById("acc_question").innerHTML = lang_flag ? "Уже зарегистрированы?" : "Already have an account? ";
+    document.getElementById("email").innerHTML = lang_flag ? "Почта" : "Email";
+    document.getElementById("first_name").innerHTML = lang_flag ? "Имя" : "First name";
+    document.getElementById("last_name").innerHTML = lang_flag ? "Фамилия" : "Last name";
+    document.getElementById("password").innerHTML = lang_flag ? "Пароль" : "Password";
+    document.getElementById("month").innerHTML = lang_flag ? "Месяц" : "Month";
+    document.getElementById("day").innerHTML = lang_flag ? "День" : "Day";
+    document.getElementById("year").innerHTML = lang_flag ? "Год" : "Year";
+    document.querySelector(".birth_p").innerHTML = lang_flag ? "Дата рождения" : "Date of birth";
+    document.getElementById("agreement").innerHTML = lang_flag ? "Нажимая кнопку Создать аккаунт, Я соглашаюсь с " : "By Create account, I agree that I have read and accepted the ";
+    document.getElementById("and_word").innerHTML = lang_flag ? " и " : " and ";
+    document.getElementById("submit").value = lang_flag ? "Создать аккаунт" : "Create account";
+    document.getElementById("russian_lang").innerHTML = lang_flag ? "РУС" : "RUS";
+    document.getElementById("eng_button").innerHTML = lang_flag ? "АНГ" : "ENG";
+    
     // Translating anchors
     let formAnchors = formDiv.getElementsByTagName("a");
-    formAnchors[0].innerHTML = "Войти";
-    formAnchors[1].innerHTML = "Пользовательскими условиями";
-    formAnchors[2].innerHTML = "Политикой Конфиденциальности";
+    formAnchors[0].innerHTML = lang_flag ? "Войти" : "Sign in";
+    formAnchors[1].innerHTML = lang_flag ? "Пользовательскими условиями" : "Terms of Use";
+    formAnchors[2].innerHTML = lang_flag ? "Политикой Конфиденциальности" : "Privacy Policy";
+}
+
+ternTranslation(isRus);
+// Translation toggling 
+ruButton.addEventListener("click", function(){
+    // Excluding unnecessary localization 
+    if (isRus == 0) {
+        isRus = 1;
+        ternTranslation(isRus);
+        engButton.classList.toggle("grey_color");
+        ruButton.classList.toggle("grey_color");
+    }
 })
 
 engButton.addEventListener("click", function() {
-    document.getElementById("page_title").innerHTML = "Form page";
-    document.getElementById("form_header").innerHTML = "Create an account";
-    document.getElementById("acc_question").innerHTML = "Already have an account?";
-    document.getElementById("email").innerHTML = "Email";
-    document.getElementById("first_name").innerHTML = "First name";
-    document.getElementById("last_name").innerHTML = "Last name";
-    document.getElementById("password").innerHTML = "Password";
-    document.getElementById("month").innerHTML = "Month";
-    document.getElementById("day").innerHTML = "Day";
-    document.getElementById("year").innerHTML = "Year";
-    document.querySelector(".birth_p").innerHTML = "Date of birth";
-    document.getElementById("agreement").innerHTML = "By Create account, I agree that I have read and accepted the ";
-    document.getElementById("and_word").innerHTML = " and ";
-    document.getElementById("submit").value = "Create account";
-    document.getElementById("russian_lang").innerHTML = "RUS";
-    document.getElementById("eng_button").innerHTML = "ENG";
-    
-    // Translating anchors
-    let formAnchors = formDiv.getElementsByTagName("a");
-    formAnchors[0].innerHTML = "Sign in";
-    formAnchors[1].innerHTML = "Terms of Use";
-    formAnchors[2].innerHTML = "Privacy Policy";
-    
+    // Excluding unnecessary localization 
+    if (isRus == 1) {
+        isRus = 0;
+        ternTranslation(isRus);
+        engButton.classList.toggle("grey_color");
+        ruButton.classList.toggle("grey_color");
+    } 
 })
-// Translating back
